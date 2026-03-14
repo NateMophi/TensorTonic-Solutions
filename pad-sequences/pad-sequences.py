574fbfb76e9d03a3b02a7aa9d57c9fd4412ee4de
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-03-14 20:43:23 +0000

    Add solution for pad-sequences

---
+++ pad-sequences/pad-sequences.py
@@ -0,0 +1,25 @@
+import numpy as np
+def pad_sequences(seqs, pad_value=0, max_len=None):
+    """
+    Returns: np.ndarray of shape (N, L) where:
+      N = len(seqs)
+      L = max_len if provided else max(len(seq) for seq in seqs) or 0
+    """
+    # Your code here
+    if len(seqs)==0:
+        return []
+    
+    if max_len != None:
+        L = max_len
+    else:
+        L = max(len(x) for x in seqs)
+
+
+    for s in seqs:
+        while len(s) < L:
+            s.append(pad_value)
+    
+    for s in seqs:
+        while len(s) > L:
+            s.pop()
+    return np.array(seqs)

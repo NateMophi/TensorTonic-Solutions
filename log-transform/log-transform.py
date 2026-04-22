bebb95e3156e34db2890399a46f27e7614b0447a
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-04-22 08:05:51 +0100

    Add solution for log-transform

---
+++ log-transform/log-transform.py
@@ -0,0 +1,9 @@
+import math
+def log_transform(values):
+    """
+    Apply the log1p transformation to each value.
+    """
+    # Write code here
+
+    y = [math.log(1+x) for x in values]
+    return y

57f64e73942ecdadeaedbe09f19d1e8f8f960232
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-04-27 09:04:56 +0100

    Update solution for bernoulli-pmf

--- bernoulli-pmf/bernoulli-pmf.py
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -6,7 +6,6 @@ def bernoulli_pmf_and_moments(x, p):
     """
     # Write code here
     x = np.array(x)
-    geo_pmf = np.where(x==1, p, 1-p)
-    geo_mean, geo_var = p, np.dot(p, 1-p)
-
-    return  geo_pmf, geo_mean, geo_var
+    pmf = np.where(x==1, p, 1-p)
+    mean, var = p, p*(1-p)
+    return (pmf, mean, var)

dcd4f2687d9a1cc80fcf6991bccb3d0ade7e7f1d
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-05-02 03:46:30 +0100

    Update solution for chi2-independence

--- chi2-independence/chi2-independence.py
+++ chi2-independence/chi2-independence.py
@@ -5,10 +5,12 @@ def chi2_independence(C):
     Compute chi-square test statistic and expected frequencies.
     """
     # Write code here
-
+    C = np.array(C)
     row_sum, col_sum = np.sum(C, axis=1), np.sum(C, axis=0)
-    N = np.sum(C)
-    E = np.outer(row_sum, col_sum)/N
+    total = np.sum(C)
+    
+    E = np.outer(row_sum, col_sum) / total
 
-    chi = np.sum((C - E)**2/E)
-    return chi, E
+    chi = np.sum((C - E)**2 / E)
+    
+    return (chi, E)

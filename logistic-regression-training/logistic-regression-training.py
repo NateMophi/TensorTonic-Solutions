1494f67f4efe31b08adc9a860609ee6de089fefe
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-05-12 02:43:59 +0100

    Update solution for logistic-regression-training

--- logistic-regression-training/logistic-regression-training.py
+++ logistic-regression-training/logistic-regression-training.py
@@ -10,19 +10,18 @@ def train_logistic_regression(X, y, lr=0.1, steps=1000):
     Return (w, b).
     """
     # Write code here
+    X = np.array(X)
+    y = np.array(y)
     n_samples, n_features = X.shape
-    N =  n_samples
-    w, b = np.zeros(n_features), 0
-    
+    N = n_samples
+    w, b = np.zeros(n_features),0
     
     for i in range(steps):
-        z = X @ w + b
-        p = _sigmoid(z) 
-
-        w_delta = np.dot(X.T, p - y) / N
-        b_delta = np.mean(p-y)
-
-        w = w - lr*w_delta
-        b = b - lr*b_delta
-
-    return w, b
+        z = X@w+ b
+        p = _sigmoid(z)
+        delta_w = np.dot(X.T, p-y ) / N 
+        delta_b = np.mean(p-y)
+        w = w - lr* delta_w
+        b = b - lr * delta_b
+        
+    return (w, b)

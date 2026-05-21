aaae0595d6efced56331173d7d29fcdfda1e6c35
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-05-21 13:38:13 +0100

    Update solution for make-diagonal

--- make-diagonal/make-diagonal.py
+++ make-diagonal/make-diagonal.py
@@ -5,13 +5,10 @@ def make_diagonal(v):
     Returns: (n, n) NumPy array with v on the main diagonal
     """
     # Write code here
-    
     n = len(v)
-    matrix = np.zeros((n, n))
-
+    M = np.zeros((n,n))
     for i in range(n):
         for j in range(n):
-            if i == j:
-                matrix[i, j]= v[i]
-
-    return matrix
+            if i==j:
+                M[i,j] = v[i]
+    return M

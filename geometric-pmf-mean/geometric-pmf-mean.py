0453c26d497d700905a08993ca39a906a59b0cbe
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-05-22 23:35:10 +0100

    Update solution for geometric-pmf-mean

--- geometric-pmf-mean/geometric-pmf-mean.py
+++ geometric-pmf-mean/geometric-pmf-mean.py
@@ -5,7 +5,7 @@ def geometric_pmf_mean(k, p):
     Compute Geometric PMF and Mean.
     """
     # Write code here
-    
-    """Q.E.D"""
     k = np.array(k)
-    return (np.dot((1-p)**(k-1), p), 1/p)
+    pmf = ((1-p)**(k-1)) * p
+    E = 1/p
+    return (pmf, E)

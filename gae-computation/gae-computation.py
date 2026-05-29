ae1190c6ff1d74be1dc30fc165c7319685fb93a1
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-05-29 21:11:37 +0100

    Update solution for gae-computation

--- gae-computation/gae-computation.py
+++ gae-computation/gae-computation.py
@@ -3,16 +3,13 @@ def gae(rewards, values, gamma, lam):
     Compute Generalized Advantage Estimation.
     """
     # Write code here
-    # TD = []
     T = len(rewards)
-    TD = [0]*T
+    Delta = []
+    A = [0]* T
     for t in range(T):
-        TD[t] = rewards[t] + gamma*values[t+1] - values[t]
-        # TD.append(delta)
-
-    A = [0]*T
-    A[-1] = TD[-1]
-    for t in range(T-2, -1, -1):
-        A[t] = TD[t] + gamma*lam*A[t+1]
-        
+        Delta.append(rewards[t] + gamma*values[t+1] - values[t])
+    A[T-1] = Delta[T-1]
+    for i in range(T-2, -1, -1):
+        A[i] = Delta[i] + gamma*lam * A[i+1]
     return A
+        

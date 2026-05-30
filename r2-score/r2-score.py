241b88979c88709c5aaeaec5137da59b1d1e8033
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-05-30 17:35:34 +0100

    Update solution for r2-score

--- r2-score/r2-score.py
+++ r2-score/r2-score.py
@@ -9,14 +9,14 @@ def r2_score(y_true, y_pred) -> float:
     """
     # Write code here
     y_true, y_pred = np.array(y_true), np.array(y_pred)
-
     
-    numerator = np.sum((y_true - y_pred)**2)
-    denominator = np.sum((y_true - np.mean(y_true))**2)
-    if denominator==0 and y_pred.all()==y_true.all():
-        return 1.0
-    elif denominator==0 and y_pred.all()!=y_true.all():
-        return 0.0
-    else:
-        R_sq = 1 - numerator/denominator
-        return R_sq
+    
+    m = np.mean(y_true)
+    num = np.sum((y_true - y_pred)**2)
+    den = np.sum((y_true - m)**2)
+    if np.max(y_true) == np.min(y_true):
+        if y_true.all() == y_pred.all():
+            return 1.0
+        else:
+            return 0.0
+    return 1 - num/den

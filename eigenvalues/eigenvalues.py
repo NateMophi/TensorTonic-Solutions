1ba2c7f5eb95c3536f50dfbb5a5c7be8c6c6b76c
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-06-21 04:09:40 +0100

    Solve eigenvalues

--- eigenvalues/eigenvalues.py
+++ eigenvalues/eigenvalues.py
@@ -4,21 +4,24 @@ def calculate_eigenvalues(matrix):
     """
     Calculate eigenvalues of a square matrix.
     """
-    # Write code here    
-    matrix = np.asarray(matrix)
-    
-    if len(matrix)==0:
+    # Write code here
+    n = len(matrix)
+    if n == 0:
         return None
+    # for i in range(n):
+    #     for j in range(i,n):
+    #         if type(matrix[j]) == int:
+    #             continue
+    #         if type(matrix[j]) == list and len(matrix[j]) != len(matrix[i]):
+    #             return None
 
-    if matrix.ndim !=2:
+    try:
+        matrix = np.array(matrix)
+        rows, cols = matrix.shape
+        if matrix.ndim <= 1 or rows != cols :
+            return None
+        
+        eig = np.linalg.eigvals(matrix)
+        return eig
+    except ValueError:
         return None
-
-    rows, cols = matrix.shape
-    if rows!=cols:
-        return None
-
-    eig_vals = np.linalg.eigvals(matrix)
-    np.lexsort(eig_vals)
-    
-    return eig_vals
-    

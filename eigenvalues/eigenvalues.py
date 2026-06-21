6c8beb4f2c75194fcaa2ed10c82e67280f3460a5
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-06-21 04:09:58 +0100

    Solve eigenvalues

--- eigenvalues/eigenvalues.py
+++ eigenvalues/eigenvalues.py
@@ -8,19 +8,11 @@ def calculate_eigenvalues(matrix):
     n = len(matrix)
     if n == 0:
         return None
-    # for i in range(n):
-    #     for j in range(i,n):
-    #         if type(matrix[j]) == int:
-    #             continue
-    #         if type(matrix[j]) == list and len(matrix[j]) != len(matrix[i]):
-    #             return None
-
     try:
         matrix = np.array(matrix)
         rows, cols = matrix.shape
         if matrix.ndim <= 1 or rows != cols :
             return None
-        
         eig = np.linalg.eigvals(matrix)
         return eig
     except ValueError:

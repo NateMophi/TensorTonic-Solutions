ed5ac307951c06db1993d19cfc88247fdffc1235
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-06-28 03:01:21 +0100

    Solve adagrad-optimizer

---
+++ adagrad-optimizer/adagrad-optimizer.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def adagrad_step(w, g, G, lr=0.01, eps=1e-8):
+    """
+    Perform one AdaGrad update step.
+    """
+    # Write code here
+    w = np.array(w)
+    g, G = np.array(g), np.array(G)
+    G_t = G + g**2
+    w_t = w - (lr/np.sqrt(G_t + eps)) * g
+    return w_t, G_t

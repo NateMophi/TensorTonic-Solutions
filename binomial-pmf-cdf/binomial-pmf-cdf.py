8bffbb6f4f0960959a092b6260f15f04b8a5f607
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-07-03 10:34:53 +0100

    Solve binomial-pmf-cdf

--- binomial-pmf-cdf/binomial-pmf-cdf.py
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -5,10 +5,9 @@ def binomial_pmf_cdf(n, p, k):
     """
     Compute Binomial PMF and CDF.
     """
-    # Write code here    
-    pmf = np.dot(np.dot(comb(n, k), p**k), (1-p)**(n-k))
+    # Write code here
+    pmf = comb(n, k) * p**k * (1-p)**(n-k)
     cdf = 0
     for i in range(k+1):
-        cdf += np.dot(np.dot(comb(n, i), p**i), (1-p)**(n-i))
-        
-    return (pmf,cdf)
+        cdf+=comb(n, i) * p**i * (1-p)**(n-i)
+    return pmf, cdf

70e93a7365a96dea6e659c5577fa3f6b08c4e192
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-07-06 11:24:47 +0100

    Solve compute-advantage

---
+++ compute-advantage/compute-advantage.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def compute_advantage(states, rewards, V, gamma):
+    """
+    Returns: A (NumPy array of advantages)
+    """
+    # Write code here
+    V = np.array(V)
+    n = len(rewards)
+    G = n * [0]
+    G[-1] = rewards[-1]
+    for t in range(n-2, -1, -1):
+        G[t] = rewards[t] + G[t+1]*gamma
+    G = np.array(G)
+    A = G - V
+    return A

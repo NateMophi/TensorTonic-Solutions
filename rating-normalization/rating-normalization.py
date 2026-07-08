31fda610508f7d51684994c8a166875c5e8cece1
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-07-08 09:14:27 +0100

    Solve rating-normalization

---
+++ rating-normalization/rating-normalization.py
@@ -0,0 +1,16 @@
+import numpy as np
+def rating_normalization(matrix):
+    """
+    Mean-center each user's ratings in the user-item matrix.
+    """
+    # Write code here
+    F =[]
+    for row in matrix:
+        filtered = [val for val in row if val!=0]
+        if len(filtered) == 0:
+            F.append(row)
+        else:
+            means = sum(filtered)/len(filtered)
+            norm = [val - means if val!=0 else 0 for val in row]
+            F.append(norm)
+    return F

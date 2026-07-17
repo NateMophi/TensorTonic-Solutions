54058bb055fbc96b46e5a109f0fb0cebd3992957
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-07-17 17:52:36 +0100

    Solve perplexity-computation

--- perplexity-computation/perplexity-computation.py
+++ perplexity-computation/perplexity-computation.py
@@ -5,12 +5,5 @@ def perplexity(prob_distributions, actual_tokens):
     # Write code here
     p = []
     n = len(actual_tokens)
-    H = 0
-    for i in range(n):
-        # for j in range(n):
-        #     if i==j:
-                # p.append(prob_distributions[i][j])
-         H += math.log(prob_distributions[i][actual_tokens[i]])/-n
-    
-    PP = math.exp(H)
-    return PP
+    H = sum([math.log(prob_distributions[i][actual_tokens[i]]) for i in range(n)])/-n
+    return math.exp(H)

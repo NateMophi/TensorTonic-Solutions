f8ebb4c16bf5b18716db11cd44a19c0addfde329
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-08-06 01:57:45 +0100

    Solve precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -2,23 +2,8 @@ def precision_recall_at_k(recommended, relevant, k):
     """
     Compute precision@k and recall@k for a recommendation list.
     """
-    recommended = recommended[:k]
-    relevant = set(relevant)
-    
-    hits = 0
-    for i in range(k):
-        if recommended[i] in relevant:
-            hits+=1
-    precison_k = hits/k
-
-    
-    rev_length = len(relevant)
-    hits2 = 0
-    for j in relevant:
-        if j in recommended:
-            hits2+=1
-    recall_k = hits2/rev_length
-    
-    return [precison_k, recall_k]
-            
-    
+    # Write code here
+    n = len(relevant)
+    precision = [recommended[i] for i in range(k) if recommended[i] in relevant]
+    precision_k, recall_k = len(precision)/k, len(precision)/n
+    return [precision_k, recall_k]

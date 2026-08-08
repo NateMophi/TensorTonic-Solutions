af086e818bb5c0ce05a05f90c6fc41514cbfcf42
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-08-08 15:34:53 +0100

    Solve baseline-predictor

--- baseline-predictor/baseline-predictor.py
+++ baseline-predictor/baseline-predictor.py
@@ -3,11 +3,10 @@ def baseline_predict(ratings_matrix, target_pairs):
     Compute baseline predictions using global mean and user/item biases.
     """
     # Write code here
-    # r = []
     non_zero_user_ratings = [[rating for rating in ratings if rating!=0] for ratings in ratings_matrix]
     non_zero_item_ratings = [[rating for rating in ratings if rating!=0] for ratings in zip(*ratings_matrix)]
     mu = sum([sum(rating)/len(rating) for rating in non_zero_user_ratings])/len(non_zero_user_ratings)
     UB = [(sum(rating)/len(rating))- mu for rating in non_zero_user_ratings]
     IB = [(sum(rating)/len(rating))- mu for rating in non_zero_item_ratings]
-    r = [mu + UB[u] + IB[i] for u, i in target_pairs]
-    return r
+    return [mu + UB[u] + IB[i] for u, i in target_pairs]
+    

697510a9d53a90010bc4818f5a4ca3200f9288af
Eyimofe Ope-Faniran <66631222+NateMophi@users.noreply.github.com>
2026-09-23 19:10:44 +0100

    Solve cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def cross_entropy_loss(y_true, y_pred):
+def cross_entropy_loss(y_true: list[int], y_pred: list[list[float]]) -> float:
     """
-    Compute average cross-entropy loss for multi-class classification.
+    Returns the mean multiclass cross-entropy loss as a Python float.
     """
     # Write code here
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     n = len(y_true)
-    p_correct = y_pred[np.arange(n), y_true]
-    return -np.mean(np.log(p_correct))
+    actual = y_pred[np.arange(n), y_true]
+    return np.mean(-np.log(actual))
+    
